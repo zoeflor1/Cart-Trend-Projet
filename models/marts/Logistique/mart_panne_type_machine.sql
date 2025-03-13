565f43a171402ad3d655f3a36f75ea6c6cdc3199
zoeflorin65@gmail.com <zoeflorin65@gmail.com>
2025-03-13 13:31:05 +0000

    Màj

--- models/marts/Logistique/mart_panne_type_machine.sql
+++ models/marts/Logistique/mart_panne_type_machine.sql
@@ -1,9 +1,10 @@
 SELECT
     type_machine,
     EXTRACT(YEAR FROM mois) AS annee,
+    EXTRACT(MONTH FROM mois) AS mois,
     COUNT(*) AS nombre_pannes,
     COUNT(DISTINCT id_machine) AS nombre_total_machines,
     ROUND(COUNT(*) / COUNT(DISTINCT id_machine), 2) AS taux_pannes_par_machine
 FROM {{ ref('stg_cartrend_entrepots_machines') }}
 WHERE etat_machine = 'En panne'
-GROUP BY annee, type_machine
+GROUP BY annee, mois, type_machine
